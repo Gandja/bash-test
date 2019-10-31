c24e975ab9c6d3b3be39998995715ae91268a4c9
Denis Gandzha <denis.gandzha@artezio.com>
2019-10-31 13:14:05 +0300

    twenty tests has done

--- test.sh
+++ test.sh
@@ -74,11 +74,11 @@ echo "   Load test - OK"
 #ls shows what is in directory
 echo -n "3. ls - "
 ls 1> "$std" 2> "$err"
-test 0 $'file.txt\ntest.sh' ""
+test 0 $'file.txt\nfile-test.txt\ntest.sh' ""
 for(( i=1;i<=$N;i++ ))
 do
 	ls 1> "$std" 2> "$err"
-	testLoad 0 $'file.txt\ntest.sh' ""
+	testLoad 0 $'file.txt\nfile-test.txt\ntest.sh' ""
 done
 echo "   Load test - OK"
 
@@ -90,49 +90,108 @@ test 0 "/usr/bin/ls" ""
 
 #5 test cat expect error
 #read file
-echo -n "5. cat - "
+echo -n "5. cat for error - "
 cat 123.txt 1> "$std" 2> "$err"
 test 1 "" "cat: 123.txt: No such file or directory"
 
-#6 test uname
+#6 test cat for open file
+echo -n "6. cat for open file - "
+echo "11" > file.txt
+cat file.txt 1> "$std" 2>"$err"
+test 0 "11" ""
+
+for(( i=1;i<=$N;i++ ))
+do
+	echo "11" > file.txt
+	cat file.txt 1> "$std" 2>"$err"
+	testLoad 0 "11" ""
+done
+echo "   Load test - OK"
+
+#7 test uname
 #return kernel's name
-echo -n "6. uname - "
+echo -n "7. uname - "
 uname -s 1> "$std" 2> "$err"
 test 0 "MINGW64_NT-10.0-18362" ""
 
-#7 test whoami
+#8 test whoami
 #return user name
-echo -n "7. whoami - "
+echo -n "8. whoami - "
 whoami 1> "$std" 2> "$err"
 test 0 "dgandzha" ""
 
-#8 test find 
+#9 test find 
 #search file
-echo -n "8. find - "
+echo -n "9. find - "
 find . -name "test*" 1> "$std" 2> "$err"
 test 0 "./test.sh" ""
 
-#9 test head
+#10 test head
 #show first line in this file
-echo -n "9. head - "
+echo -n "10. head - "
 head -n 1 test.sh 1> "$std" 2> "$err"
 test 0 "#!/bin/bash" ""
 
-#10 test pwd
+#11 test pwd
 #show directory
-echo -n "10. pwd - "
+echo -n "11. pwd - "
 pwd 1> "$std" 2> "$err"
-test 0 "/c/Linux" ""
+test 0 "/c/Linux/bash-test" ""
 
-#11 test wc
+#12 test wc
 #show count of word,string,byte,symbols
-echo -n "11. wc - "
+echo -n "12. wc - "
 wc file.txt 1> "$std" 2>"$err"
-test 0 "0 0 0 file.txt" ""
+test 0 "1 1 3 file.txt" ""
 
-#12 test where is
-#show path fo file/directory
-echo -n "12. whereis - "
-where is file.txt 1>"$std" 2>"$err"
-test 0 "C:\Linux\file.txt" ""
+#13 test touch
+echo -n "13. touch - "
+FILE=/c/Linux/bash-test/file-test.txt
+touch $FILE 1> "$std" 2> "$err"
+test 0 "" ""
+ 
+#14 test grep
+echo -n "14. grep - "
+echo -e "1\n5\n5\n5\n3" > $FILE
+grep "5" $FILE 1> "$std" 2> "$err"
+test 0 $'5\n5\n5' ""
 
+for(( i=1;i<=$N;i++ ))
+do
+	grep "5" $FILE 1> "$std" 2> "$err"
+	testLoad 0 $'5\n5\n5' ""
+done
+echo "    Load test - OK"
+
+#15 test mkdir
+echo -n "15. mkdir - "
+mkdir -p /c/Linux/a/b 1> "$std" 2>"$err"
+test 0 "" ""
+
+#16 test date
+echo -n "16. date -  "
+date +%Y 1> "$std" 2> "$err"
+test 0 "2019" ""
+
+#17 test - date for error
+echo -n "17. date fo error - "
+date YY 1> "$std" 2> "$err"
+test 1 "" "date: invalid date ‘YY’"
+
+#18 test tail 
+echo -n "18. tail -  "
+tail -n 1 test.sh 1> "$std" 2> "$err"
+test 0 "finish=1" ""
+
+#19 test rm  
+echo -n "19. rm - "
+tmp=$(mktemp)
+rm $tmp 1> "$std" 2> "$err"
+test 0 "" ""
+
+#20 test file
+echo -n "20. file -"
+file file.txt 1> "$std" 2> "$err"
+test 0 "file.txt: ASCII text" ""
+
+finish=1
